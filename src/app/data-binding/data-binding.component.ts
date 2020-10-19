3ea363fbfac850d2bf7a1898d481aafde855580d
Luis Augusto <luisaugustosb93@gmail.com>
2020-10-19 09:59:46 -0300

    Criação de Componentes,Rotas e Databindind

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+  numero: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  obtemNumero(numero: number){
+    this.numero = numero;
+  }
+}
